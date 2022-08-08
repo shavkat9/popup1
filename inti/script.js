a3066c92e060ef0013dc942f36cd8a2ac5ed3958
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-08-09 00:28:42 +0500

    popup
    popup btn

---
+++ inti/script.js
@@ -0,0 +1,31 @@
+// let btn = document.querySelector('button');
+
+// btn.addEventListener('keypress', (e) => {
+//     alert("save yana")
+//     // let btn = e.target;
+//     // btn.innerHTML = 'bosildi'
+// })
+
+
+// let age = 21;
+
+// if(age < 20) {
+//     console.log('Siz yosh emassiz endi')
+// }
+// else{
+//     console.log('Siz yetarlicha yoshdasiz')
+// }
+
+// let x = 2;
+// let y = 4;
+
+// y = 5
+
+// let res = x + y;
+
+// console.log(res)
+
+
+let res = 60;
+
+let maxNum = res > 60 ? 'Togri' : 'yo\'q'
